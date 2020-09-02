1e57896cebb91f7a4c0d99c514c1923912e1e78e
sohumgautam17 <sohumgautam@icloud.com>
2020-09-01 20:39:27 -0400

    Angry Bird modified

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,8 +36,16 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    
     bird = new Bird(100,100);
 
+
+    log6 = new Log(230, 180, 80, PI/2);
+    chain=new Chain(bird.body,log6.body);
+
+
+
+   
 }
 
 function draw(){
@@ -61,4 +71,8 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    chain.display();
+
+    
 }
